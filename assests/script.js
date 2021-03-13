86c015fb44011279e96e27ca05660c5e4297affb
Breon Sparrow <beyond.committed@gmail.com>
2021-03-13 14:27:54 -0700

    resume js

--- assests/script.js
+++ assests/script.js
@@ -1,12 +1,14 @@
 function getResume() {
 var myUl = document.querySelector("#myUl");
-var resume = document.createElement("li");
-resume.setAttribute("href", "https://www.linkedin.com/in/breon-sparrow-31bb24205/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAADRQUscBiPDhB2YtiVJeBs5gYKGQ1aKh2XM,1635450821416)/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BTt9QF4cOQ9asffCWAfPSmA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base-featured_item_detail_view");
-resume.innerHTML = "Resume";
-myUl.appendChild(resume);
+var resumeLi = document.createElement("li");
+var aTag = document.createElement("a")
+aTag.setAttribute("href", "https://www.linkedin.com/in/breon-sparrow-31bb24205/detail/overlay-view/urn:li:fsd_profileTreasuryMedia:(ACoAADRQUscBiPDhB2YtiVJeBs5gYKGQ1aKh2XM,1635450821416)/?lipi=urn%3Ali%3Apage%3Ad_flagship3_profile_view_base%3BTt9QF4cOQ9asffCWAfPSmA%3D%3D&licu=urn%3Ali%3Acontrol%3Ad_flagship3_profile_view_base-featured_item_detail_view");
+aTag.innerHTML = "Resume";
+myUl.appendChild(resumeLi);
+resumeLi.append(aTag);
 }
 getResume();
 
-button.onclick = function() {
-    getResume();
-}
+// button.onclick = function() {
+//     getResume();
+// }
